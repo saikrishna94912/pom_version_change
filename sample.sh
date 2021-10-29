9b658d9e464e73af1ea25a7f0758d3541594e264
saikrishna94912 <55633824+saikrishna94912@users.noreply.github.com>
2021-10-30 03:48:09 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-open pom.xml ;
-register-namespace m http://maven.apache.org/POM/4.0.0 ;
-for //m:version
-    set . xsh:subst(., '(?<=\.)([0-9]+)$', '$1+1', 'e') ;
-save :b ;
+version=$(grep -ri "<version>" pom.xml |head -n 1 | sed -e 's/^[ \t]*<version>\([^<]*\)<.*$/\1/')
+
+major=$(echo $version | cut -c1)
+var2=$(echo $version | cut -c2)
+miner=$(echo $version | cut -c3)
+var4=$(echo $version | cut -c4)
+bug=$(echo $version | cut -c5)
+bug=$((bug+1))
+str="-SNAPSHOT"
+incrementVer=$major$var2$miner$var4$bug$str
+echo $incrementVer
+
+LN=$(grep -n "<version>" pom.xml | head -1 | awk -F ":" '{print $1}') sed -i "$LN s/$version/$incrementVer/" pom.xml
+echo $LN
