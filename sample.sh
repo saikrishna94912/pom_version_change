ad04462e276b835eab895f6b7ef2d5148de5387f
saikrishna94912 <55633824+saikrishna94912@users.noreply.github.com>
2021-10-30 13:07:29 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -14,4 +14,5 @@ echo $incrementVer
 
 LN=$(grep -n "<version>" pom.xml | head -1 | awk -F ":" '{print $1}') sed -i "$LN s/$version/$incrementVer/" pom.xml
 echo $LN
-sed -i "s/\(<version>\)[^<]*\(<\/version>\)/\1$LN\2/" pom.xml
+mvn versions:set -DnewVersion=LN
+mvn versions:set -DnewVersion=echo $LN
