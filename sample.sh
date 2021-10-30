c7d10f559e29fdf111aba12adc69e1404c95a0a8
saikrishna94912 <55633824+saikrishna94912@users.noreply.github.com>
2021-10-30 15:14:14 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -15,4 +15,4 @@ echo $incrementVer
 LN=$(grep -n "<version>" pom.xml | head -1 | awk -F ":" '{print $1}') sed -i "$LN s/$version/$incrementVer/" pom.xml
 echo $LN
 echo $(grep -n "<version>" pom.xml | head -1 | awk -F ":" '{print $1}') sed -i "$LN s/$version/$incrementVer/" pom.xml
-
+echo $LN
