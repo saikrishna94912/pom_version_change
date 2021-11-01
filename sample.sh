a361e1108db7a2d79c0403e01c78ed9d328d5941
saikrishna94912 <sai.ainavilli@gmail.com>
2021-11-01 10:36:15 +0530

    version1

--- sample.sh
+++ sample.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 version=$(grep -ri "<version>" pom.xml |head -n 1 | sed -e 's/^[ \t]*<version>\([^<]*\)<.*$/\1/')
-
-major=$(echo $version | cut -c1)
-var2=$(echo $version | cut -c2)
-miner=$(echo $version | cut -c3)
-var4=$(echo $version | cut -c4)
-bug=$(echo $version | cut -c5)
-bug=$((bug+1))
-str="-SNAPSHOT"
-incrementVer=$major$var2$miner$var4$bug$str
+echo $version
+latest=$(echo $version | cut -d "." -f 1)
+latest=$((latest+1))
+echo $latest
+major=$(echo $version | cut -d "." -f 2)
+echo $major
+minor=$(echo $version | cut -d "." -f 3)
+echo $minor
+str="."
+incrementVer=$latest$str$major$str$minor
 echo $incrementVer
 
 LN=$(grep -n "<version>" pom.xml | head -1 | awk -F ":" '{print $1}') sed -i "$LN s/$version/$incrementVer/" pom.xml
